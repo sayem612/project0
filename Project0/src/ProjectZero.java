68e9c1e227d4ca3cd263829d2a71f46c4da8bc05
sayem612 <sayemedu@yahoo.com>
2015-09-17 11:57:03 -0700

    Update ProjectZero.java

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -8,8 +8,8 @@ public class ProjectZero {
 			int input1, input2, result;
 			
 			System.out.print("Please enter two numbers of your exalted choosing: ");
-			input1 = inputSource.nextInt();
-			input2 = inputSource.nextInt();
+			input1 = inputSource.nextInt();//users enters number 1
+			input2 = inputSource.nextInt();//users enters number 2
 			
 			result = input1 + input2;
 			System.out.println(result);
